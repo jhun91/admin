0b4e9c95cf8e34f86cf4ca288ab13741119ec8b5
hun <fonercis@naver.com>
2020-04-23 20:36:06 +0900

    기본 CRUD 테스트 코드 진행

---
+++ src/test/java/com/tistory/fonercis/admin/repository/UserRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.tistory.fonercis.admin.repository;
+
+import com.tistory.fonercis.admin.AdminApplicationTests;
+import com.tistory.fonercis.admin.model.entity.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserRepositoryTest extends AdminApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        User user = new User();
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-3333-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("TestUser03");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
+    }
+
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
+
+
+    }
+
+    @Test
+    @Transactional
+    public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void delete() {
+        Optional<User> user = userRepository.findById(3L);
+
+        assertTrue(user.isPresent());
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        assertFalse(deleteUser.isPresent());
+    }
+
+}
